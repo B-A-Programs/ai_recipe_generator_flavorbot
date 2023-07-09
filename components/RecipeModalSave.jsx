f2e3d7eab3fb3ec25cbc44602988c4fc7cd60717
Andrei Tudorache <tudoracheandrei09@gmail.com>
2023-07-09 17:14:05 +0300

    Recipe Modal

---
+++ components/RecipeModalSave.jsx
@@ -0,0 +1,79 @@
+import { Dialog, Transition } from '@headlessui/react'
+import Image from 'next/image'
+import { Fragment } from 'react'
+
+const RecipeModalSave = ({ recipe, setRecipe }) => {
+  const unfilteredIngredients = recipe.analyzedInstructions[0].steps.map((step) => (step.ingredients.map((ingredient) => (ingredient.name)))).flat()
+
+  /// Remove duplicates from ingredients array
+  const ingredients = [...new Set(unfilteredIngredients)]
+  const instructions = recipe.analyzedInstructions[0].steps.map((step) => (step.step))
+  const image = recipe.image
+  const title = recipe.title
+
+  const handleSave = () => {
+
+  }
+
+  return (
+    <>
+        <Transition appear show={true} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={() => setRecipe([])}>
+                <Transition.Child as={Fragment} enter="ease-out duration-300"
+                enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200"
+                leaveFrom="opacity-100" leaveTo="opacity-0"
+                >
+                    <div className='fixed inset-0 bg-black bg-opacity-25' />
+                </Transition.Child>
+
+                <div className='fixed inset-0 overflow-y-auto'>
+                    <div className='flex min-h-full items-center justify-center p-4 text-center'>
+                    <Transition.Child as={Fragment} enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95"
+                    >
+                        <Dialog.Panel className="relative w-2/5 max-h-[90vh] overflow-y-auto tarnsform rounded-2xl bg-white p-6 text-left shadow-gray-500 shadow-xl transition-all flex flex-col gap-5">
+                            <button type="button" onClick={() => setRecipe([])} className='absolute top-2 right-2 z-10 w-fit p-2 bg-amber-500 rounded-full'>
+                                <Image src="/close.png" alt="close" width={20} height={20} className='object-contain' />
+                            </button>
+
+                            <div className='flex-1 flex flex-col gap-3'>
+                                <div className='relative w-full mx-auto h-52 bg-[url("/pattern.jpg")] bg-cover bg-center rounded-lg'>
+                                    <Image src={`${image}`} alt="food image" fill priority className="object-contain" />
+                                </div>
+                            </div>
+
+                            <div className='flex-1 flex flex-col gap-2'>
+                                <h2 className='font-bold text-xl text-white bg-gradient-to-r py-3 px-6 mx-auto rounded-full from-amber-700 to-orange-400 capitalize'>
+                                    {title}
+                                </h2>
+
+                                <div className='mt-3 flex flex-wrap gap-4 capitalize text-stone-700'>
+                                    <b>Ingredients: </b>{ingredients.map((ingredient) => (<>{`${ingredient}, `}</>))}
+                                </div>
+
+                                <div className='mt-3 flex flex-col gap-3 text-stone-700'>
+                                    <b>Instructions:<br /> </b>{instructions.map((instruction, index) => (<div><span className='font-semibold'>{`Step ${index + 1}: `}</span>{`${instruction}`}</div>))}
+                                </div>
+
+                                <div className='flex-between mt-6'>
+                                    <a href={`${recipe.sourceUrl}`} className='bg-blue-600 px-4 py-2 text-white rounded-xl'>
+                                        Original Recipe
+                                    </a>
+
+                                    <button onClick={handleSave} className='bg-green-700 px-4 py-2 text-white rounded-xl'>
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
+                        </Dialog.Panel>
+                    </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    </>
+  )
+}
+
+export default RecipeModalSave
--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -1,45 +1,83 @@
 'use client'
 
-import { useState, Fragment } from 'react'
+import { useState, Fragment, useEffect } from 'react'
 import cuisines from '@constants/cuisines'
 import CuisineComboBox from './CuisineComboBox';
 import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import RecipeModalSave from './RecipeModalSave';
 
 
 const SearchManufacturer = () => {
-  const [cuisine, setCuisine] = useState('');
-  const [query, setQuery] = useState('');
-  const { data: session, status } = useSession()
+    const [cuisine, setCuisine] = useState('');
+    const [query, setQuery] = useState('');
+    const [message, setMessage] = useState('');
+    const [recipe, setRecipe] = useState([]);
 
-  const filteredCuisines = query === "" 
-    ? cuisines 
-    : cuisines.filter((item) => (
-      item.toLowerCase().replace(/\s+/g, "")
-      .includes(query.toLowerCase().replace(/\s+/g, ""))
-    ))
+    const { data: session, status } = useSession();
 
-  return (
-    <>
-        {session?.user ?
-        <form className='mt-14 flex-center gap-12'>
-            <div className='flex-center flex-col gap-2'>
-                <label for="query" className='font-extrabold font-inter text-stone-600 text-xl'>Recipe description</label>
-                <input id="query" className='search-cuisine__input' placeholder="Egs: chicken, pizza diavola, etc." type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
-            </div>
-            <div className='flex-center flex-col gap-2'>
-                <label for="cuisine" className='font-extrabold font-inter text-stone-600 text-xl'>Cuisine</label>
-                <CuisineComboBox cuisine={cuisine} setCuisine={setCuisine} id="cuisine" />
-            </div>
+    const filteredCuisines = query === ""
+        ? cuisines
+        : cuisines.filter((item) => (
+            item.toLowerCase().replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        ))
 
-            <button type="submit" className='mt-3 rounded-full bg-gradient-to-r from-orange-500 to-amber-500 text-white text-lg font-bold py-3 px-8 flex gap-2'><Image src="search.svg" width={25} height={25} alt="search icon" />Search</button>
-        </form>
-        : 
-        <div className='mt-12 text-xl text-stone-500 font-extrabold'>
-            Please log in to search for recipes.
-        </div>}
-    </>
-  )
+    const initRecipe = async () => {
+        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${query}&number=6&addRecipeInformation=true&cuisine=${cuisine}`)
+
+        const result = await response.json()
+
+        if(result.results.length === 0) {
+            setMessage('No results found. Please try again.')
+            setRecipe([])
+            return;
+        }
+
+        setRecipe(await result.results[(Math.floor(Math.random() * result.results.length))])
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setMessage('');
+
+        if (query === "" || cuisine === "") {
+            setMessage('Please fill in all the fields.');
+            return;
+        }
+
+        await initRecipe()
+
+        setCuisine('')
+        setQuery('')
+    }
+
+    return (
+        <>
+            {session?.user ?
+                <form className='mt-14 flex-center gap-12' onSubmit={handleSubmit}>
+                    <div className='flex-center flex-col gap-2'>
+                        <label htmlFor="query" className='font-extrabold font-inter text-stone-600 text-xl'>Recipe description</label>
+                        <input id="query" className='search-cuisine__input' placeholder="Egs: chicken, pizza, pasta etc." type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+                    </div>
+                    <div className='flex-center flex-col gap-2'>
+                        <label htmlFor="cuisine" className='font-extrabold font-inter text-stone-600 text-xl'>Cuisine</label>
+                        <CuisineComboBox cuisine={cuisine} setCuisine={setCuisine} id="cuisine" />
+                    </div>
+
+                    <button type="submit" className='mt-3 rounded-full bg-gradient-to-r from-orange-500 to-amber-500 text-white text-lg font-bold py-3 px-8 flex gap-2'><Image src="search.svg" width={25} height={25} alt="search icon" />Search</button>
+                </form>
+                :
+                <div className='mt-12 text-xl text-stone-500 font-extrabold'>
+                    Please log in to search for recipes.
+                </div>
+            }
+
+            {recipe?.length !== 0 && recipe != undefined && <RecipeModalSave recipe={recipe} setRecipe={setRecipe} />}
+
+            {message && <div className='mt-16 error'>{message}</div>}
+        </>
+    )
 }
 
 export default SearchManufacturer
