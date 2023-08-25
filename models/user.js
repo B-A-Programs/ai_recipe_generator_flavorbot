e02bec03536cbcc29d2d2b3e844c97d377cf3e22
Andrei Tudorache <tudoracheandrei09@gmail.com>
2023-08-25 20:47:57 +0300

    User fix

--- models/user.js
+++ models/user.js
@@ -3,8 +3,7 @@ import { Schema, model, models } from "mongoose"
 const UserSchema = new Schema({
     username: {
         type: String,
-        required: [true, 'Username is required!'],
-        match: [/^(?=.{2,24}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 2-24 alphanumeric letters and be unique!"]
+        required: [true, 'Username is required!']
     },
     email: {
         type: String,
