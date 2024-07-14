a98254e982cdba66f235858e9a42063cf47c6e56
Andrei Tudorache <tudoracheandrei09@gmail.com>
2024-07-14 13:19:02 +0300

    Api update bugfix

--- app/api/apirecipe/route.js
+++ app/api/apirecipe/route.js
@@ -4,12 +4,12 @@ export const POST = async (req) => {
     try {
         let response;
         if(query === "")
-            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=a&number=6&addRecipeInformation=true&cuisine=${cuisine}`)
+            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=a&number=6&addRecipeInformation=true&cuisine=${cuisine}&addRecipeInstructions=true`)
 
         if(cuisine === "") 
-            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${query}&number=6&addRecipeInformation=true`)
+            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${query}&number=6&addRecipeInformation=true&addRecipeInstructions=true`)
         else
-            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${query}&number=6&addRecipeInformation=true&cuisine=${cuisine}`)
+            response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY}&query=${query}&number=6&addRecipeInformation=true&cuisine=${cuisine}&addRecipeInstructions=true`)
 
         response = await response.json()
 
--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       serverComponentsExternalPackages: ["mongoose"],
     },
     images: {
-      domains: ['lh3.googleusercontent.com', 'spoonacular.com'],
+      domains: ['lh3.googleusercontent.com', 'spoonacular.com', 'img.spoonacular.com'],
     },
     webpack(config) {
       config.experiments = {
